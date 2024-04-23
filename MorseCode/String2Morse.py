1fbf009996d1f3a3b2814c7ec7bcabc37f6ac237
PiyushChall <challpiyush79@gmail.com>
2024-04-23 13:30:04 +0530

    added the code

---
+++ MorseCode/String2Morse.py
@@ -0,0 +1,21 @@
+import json
+from typing import Dict
+
+def load_data():
+    with open("MorseDictionary.json", "r") as f:
+        data: dict = json.load(f)
+        return data
+
+def generate_morse_code_from_string(data,text):
+    morse_code = ' '.join([data[char] if char in data else char for char in text])
+    return morse_code
+
+def main():
+    data = load_data()
+    input_string = input("Enter your desires to convert into MorseCode: ")
+    code = generate_morse_code_from_string(data, input_string)
+    print(code)
+
+
+if __name__ == "__main__":
+    main()
